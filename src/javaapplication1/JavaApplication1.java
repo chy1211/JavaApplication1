2a7d536f86d91ae7dd1ca231b1004c969a11ecd5
chy1211 <henry70304@gmail.com>
2022-09-28 02:55:20 +0800

    week2HW

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,28 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // variable's life cycle: declaration -> allocaation -> initialization -> reclaim
+        System.out.println("HelloWorld");
+        
+        int i = 3;
+        System.out.println(i);
+        
+        int grade = 70;
+        if(grade>=60){
+            System.out.println("pass");
+        }else{
+            System.out.println("failed");
+        }
+        
+        int l =0;
+        for (int j=1;j<=10;j++){
+            if(j%4==0 || j%4==1){
+               l=l+j;
+           }else{
+               l=l-j;
+           }
+        }
+        System.out.println(l);
     }
     
 }
